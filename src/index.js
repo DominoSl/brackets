8fa057966cb825deaab04748fd6a9fafc2a8ebfa
DominoSL <artemenko.mik@gmail.com>
2023-01-16 08:53:41 +0200

    feat: solution omplemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = {};
+
+  for (const pair of bracketsConfig) {
+    config[pair[0]] = pair[1];
+  }
+
+  const result = [];
+
+  for (const bracket of str) {
+    if (bracket === config[result[result.length - 1]]) {
+      result.pop();
+    } else {
+      result.push(bracket);
+    }
+  }
+
+  return result.length === 0;
 }
